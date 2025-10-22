ca2b439e61cc650fdd933b373f281ffd057d9e65
Olek Raymond <123381967+OlekRaymond@users.noreply.github.com>
2025-10-22 12:30:21 +0000

    Add external tests for cpp-utilities

---
+++ tests/external_tests/Martchus-cpp-utilities.cmake
@@ -0,0 +1,18 @@
+
+set(BUILD_TESTING ON)
+set(EXCLUDE_TESTS_FROM_ALL OFF)
+
+# pull the code we want to test
+FetchContent_Declare(
+    cpputils
+    GIT_REPOSITORY https://github.com/OlekRaymond/cpp-utilities.git
+    GIT_TAG        cppunit-removal
+)
+# Currently using a fork with no changes.
+#  TODO: update to use source repo and release tags
+
+# Make it available
+FetchContent_MakeAvailable(cpputils)
+
+# Use ctest to run
+#  (nothing required)
