e8b6c56a52cd5988071f0bff6c47e0039bc75421
Olek Raymond <123381967+OlekRaymond@users.noreply.github.com>
2025-10-22 16:48:38 +0000

    #33 fixes CI on windows

--- tests/external_tests/Martchus-cpp-utilities.cmake
+++ tests/external_tests/Martchus-cpp-utilities.cmake
@@ -3,7 +3,16 @@ set(BUILD_TESTING ON)
 set(EXCLUDE_TESTS_FROM_ALL OFF)
 
 if (WIN32)
+    # Attempts to reduce features to allow it to work on CI
     set(USE_ICONV OFF)
+    set(USE_BOOST_PROCESS OFF)
+    set(USE_NATIVE_FILE_BUFFER ON)
+    set(FORCE_BOOST_IOSTREAMS_FOR_NATIVE_FILE_BUFFER OFF)
+    # Either boost or gcc is needed so it doesn't work on CI
+    if ("$ENV{CI}")
+        message(STATUS "Cannot build cpp-utilites without boost or gnu, skipping test")
+        return()
+    endif()
 endif()
 
 # pull the code we want to test
