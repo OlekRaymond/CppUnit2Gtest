d56796345ae4f1789504fbe791bc6d6860f34efa
Olek Raymond <123381967+OlekRaymond@users.noreply.github.com>
2025-10-22 16:22:49 +0000

    #33 Changes required for external tests

--- tests/external_tests/Martchus-cpp-utilities.cmake
+++ tests/external_tests/Martchus-cpp-utilities.cmake
@@ -2,13 +2,17 @@
 set(BUILD_TESTING ON)
 set(EXCLUDE_TESTS_FROM_ALL OFF)
 
+if (WIN32)
+    set(USE_ICONV OFF)
+endif()
+
 # pull the code we want to test
 FetchContent_Declare(
     cpputils
     GIT_REPOSITORY https://github.com/OlekRaymond/cpp-utilities.git
     GIT_TAG        cppunit-removal
 )
-# Currently using a fork with no changes.
+# Currently using a fork with small changes.
 #  TODO: update to use source repo and release tags
 
 # Make it available
